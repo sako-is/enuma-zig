6db2224cce56ca9783154dc625db1fc65e2dbec4
Said <isazadasaid@gmail.com>
2022-11-03 13:12:39 +0000

    .gitmodules

--- engine/core/app.zig
+++ engine/core/app.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
+const glfw = @import("glfw");
 
 const Engine = struct {
     x: u16 = 100, 
@@ -10,4 +11,26 @@ const Engine = struct {
 };
 
 pub fn create_engine(x: u16, y: u16, width: u16, height: u16, name: []const u8) Engine {
+    Engine engine = {
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+        .name = name
+    };
+
+    return engine;
+}
+
+pub fn start_engine(*Engine engine) !void {
+    try glfw.init(.{});
+    defer glfw.terminate();
+
+    const window =
+        try glfw.Window.create(engine.width, engine.height, engine.name, null, null, .{});
+    defer window.destroy();
+    
+    while (!window.shouldClose()) {
+        try glfw.pollEvents();
+    }
 }
